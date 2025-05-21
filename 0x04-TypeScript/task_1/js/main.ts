1068ca8427f5c9ce07482f79b2d0991ca463fe41
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-21 12:16:56 +0200

    feat: create StudentInterface interface, StudentConstructor interface and implement StudentInterface into StudentClass

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -42,3 +42,32 @@ const printTeacher = (firstName: string, lastName: string) => {
 };
 
 console.log(printTeacher("Morgan", "Stanley"));
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
