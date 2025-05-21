1c98a3405baefa29c1f183f043ca4a735091305b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-21 10:38:39 +0200

    feat: add Student interface, created two students and created an array containing to student variables

--- 0x04-TypeScript/task_0/src/main.ts
+++ 0x04-TypeScript/task_0/src/main.ts
@@ -1,1 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
 
+const firstStudent: Student = {
+  firstName: "Alexis",
+  lastName: "Smith",
+  age: 23,
+  location: "New York",
+};
+
+const secondStudent: Student = {
+  firstName: "Garry",
+  lastName: "Nagamoto",
+  age: 26,
+  location: "Tokyo",
+};
+
+const studentList: Student[] = [firstStudent, secondStudent];
