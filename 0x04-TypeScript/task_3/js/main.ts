5104578690ce558fe19b7b2b9e8ef24035ae1370
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-22 13:33:54 +0200

    feat: In main.ts, at the top of the file create a triple slash directive that includes all the dependencies from crud.d.ts. Import the RowID type and RoeElement from interface. Import everything from crud.js as CRUD

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,20 @@
+/// <reference path="./crud.d.ts"/>
+
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud.js";
+
+export function insertRow(row: RowElement): RowID {
+  console.log("Insert row", row);
+  return Math.floor(Math.random() * Math.floor(1000));
+}
+
+export function deleteRow(rowId: RowID): void {
+  console.log("Delete row id", rowId);
+  return;
+}
+
+export function updateRow(rowId: RowID, row: RowElement): RowID {
+  console.log(`Update row ${rowId}`, row);
+
+  return rowId;
+}
