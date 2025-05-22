aca5770939a53b4fe2edffbea3cec07e53a268c4
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-22 10:40:58 +0200

    feat: add class Director and implement DirectorInterface

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -9,3 +9,15 @@ interface TeacherInterface {
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
