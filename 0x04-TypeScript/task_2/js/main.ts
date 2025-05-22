e556349165174f6f926af48afa9db5e158eec735
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-22 10:43:48 +0200

    feat: add class Teacher and implement TeacherInterface

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -21,3 +21,15 @@ class Director implements DirectorInterface {
     return "Getting to director tasks";
   }
 }
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
